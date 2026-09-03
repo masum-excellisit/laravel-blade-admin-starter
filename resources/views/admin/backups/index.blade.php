f1235d9abe624bdac2b3f95499a8b63d67559261
Sk Masum Ali <masum@excellisit.net>
2026-09-03 17:10:49 +0530

    Fix admin QA bugs: backups index

--- resources/views/admin/backups/index.blade.php
+++ resources/views/admin/backups/index.blade.php
@@ -1,8 +1,1 @@
-@extends('layouts.admin')
-@section('title', 'Backups')
-@section('content')
-@php
-    $partLabels = ['database' => 'Database', 'storage' => 'Storage', 'code' => 'Codebase'];
-@endphp
-<div>LOADING_FULL_CONTENT_NEXT</div>
-@endsection
+file:///tmp/qa-backups-plain.txt
