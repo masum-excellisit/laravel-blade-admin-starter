aaab43836f182699749ffb68b70799815b012549
Sk Masum Ali <masum@excellisit.net>
2026-09-03 16:38:43 +0530

    Fix admin QA bugs: admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -109,6 +109,7 @@
         </header>
 
         <main class="p-4 sm:p-6 lg:p-8 max-w-[1600px] mx-auto">
+            <x-validation-summary />
             @yield('content')
         </main>
     </div>
